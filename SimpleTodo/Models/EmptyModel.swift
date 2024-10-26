5be019a59f8cdcfc80c26eafbf5db0498728b993
Belal Samy <belalsamy10@gmail.com>
2024-10-26 16:26:05 +0300

    💨 Added EmptyModel to handle requests, that return nothing

---
+++ SimpleTodo/Models/EmptyModel.swift
@@ -0,0 +1,12 @@
+//
+//  EmptyModel.swift
+//  SimpleTodo
+//
+//  Created by Belal Samy on 10/26/24.
+//
+
+import Foundation
+
+struct EmptyModel: Codable {
+    // Yup, it's empty, nothing here ...
+}
