f7e0ca2d1184200b2c15b41af859bd51b9ebe3d6
Belal Samy <belalsamy10@gmail.com>
2024-10-26 18:05:59 +0300

    🎮 Added Home View properties & methods to View Model Type Protocols

--- SimpleTodo/Screens/Home/HomeViewModel.swift
+++ SimpleTodo/Screens/Home/HomeViewModel.swift
@@ -8,16 +8,30 @@
 import Foundation
 import Combine
 
+//MARK: - Delegate
+
+protocol HomeViewModelDelegate: AnyObject {
+    func reloadData()
+    func didExitWithError(error: String)
+}
+
 //MARK: - View Model
 
 class HomeViewModel {
+    var search = CurrentValueSubject<String, Never>("")
+    var placeholders = CurrentValueSubject<[TodoListItemModel], Never>([])
+    var todos = CurrentValueSubject<[TodoListItemModel], Never>([])
+    var errorMessage = CurrentValueSubject<String, Never>("")
+    var isLoading = CurrentValueSubject<Bool, Never>(false)
     
     //MARK: - Init
-     
+    
+    private var delegate: HomeViewModelDelegate
     private var coordinator: HomeCoordinator
     private var repository: TodoRepository
-     
-    init(coordinator: HomeCoordinator, repository: TodoRepository = DefaultTodoRepository()) {
+    
+    init(delegate: HomeViewModelDelegate, coordinator: HomeCoordinator, repository: TodoRepository = DefaultTodoRepository()) {
+        self.delegate = delegate
         self.coordinator = coordinator
         self.repository = repository
     }
@@ -25,9 +39,140 @@ class HomeViewModel {
 
 //MARK: - Input
 
-extension HomeViewModel: HomeViewModelInput {}
+extension HomeViewModel: HomeViewModelInput {
+    
+    // CRUD Operations
+    
+    func getPlaceholders(search: String = "") {
+        isLoading.send(true)
+        repository.fetchPlaceholders(search: search) { [weak self] result in
+            guard let self = self else { return }
+            self.isLoading.send(false)
+            switch result {
+            case .success(let placeholders):
+                self.placeholders.send(placeholders)
+                self.delegate.reloadData()
+                 
+            case .failure(let error):
+                self.delegate.didExitWithError(error: error.value)
+            }
+        }
+    }
+     
+    func getTodoList(search: String = "") {
+       isLoading.send(true)
+       repository.fetchTodos(search: search) { [weak self] result in
+           guard let self = self else { return }
+           self.isLoading.send(false)
+           switch result {
+           case .success(let todos):
+               self.todos.send(todos)
+               self.delegate.reloadData()
+               
+           case .failure(let error):
+               self.delegate.didExitWithError(error: error.value)
+           }
+       }
+    }
+     
+    func addTodo(_ todo: TodoListItemModel) {
+        repository.addTodo(todo) { [weak self] result in
+            switch result {
+            case .success:
+                self?.todos.value.append(todo)
+                self?.getTodoList()
+                 
+            case .failure(let error):
+                self?.delegate.didExitWithError(error: error.value)
+            }
+        }
+    }
+     
+    func toggleCompleted(_ todo: TodoListItemModel, isCompleted: Bool) {
+        repository.updateTodo(todo, markAsCompleted: !isCompleted) { [weak self] result in
+            switch result {
+            case .success:
+                self?.getTodoList()
+                 
+            case .failure(let error):
+                self?.delegate.didExitWithError(error: error.value)
+            }
+        }
+    }
+     
+    func deleteTodo(_ todo: TodoListItemModel) {
+        repository.removeTodo(todo) { [weak self] result in
+            switch result {
+            case .success:
+                self?.getTodoList()
+                
+            case .failure(let error):
+                self?.delegate.didExitWithError(error: error.value)
+            }
+        }
+    }
+}
 
 //MARK: - Output
 
-extension HomeViewModel: HomeViewModelOutput {}
-
+extension HomeViewModel: HomeViewModelOutput {
+    var isEmpty: Bool {
+        return todos.value.isEmpty
+    }
+    
+    var numberOfSections: Int {
+        return 2
+    }
+     
+    func headerTitle(section: Int) -> String? {
+        switch section {
+        case 0:
+            return "My Todos (Local)"
+        case 1:
+            return "Examples (Remote)"
+        default:
+            return nil
+        }
+    }
+     
+    func numberOfCells(section: Int) -> Int {
+        switch section {
+        case 0:
+            return todos.value.count
+        case 1:
+            return placeholders.value.count
+        default:
+            return 0
+        }
+    }
+     
+    func heightForRow(section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 50
+        default:
+            return 0
+        }
+    }
+     
+    func todo(section: Int, index: Int) -> TodoListItemModel? {
+        switch section {
+        case 0:
+            return todos.value.get(at: index)
+        case 1:
+            return placeholders.value.get(at: index)
+        default:
+            return nil
+        }
+    }
+     
+    func restartPlaceholders() {
+        placeholders.send([])
+        getPlaceholders()
+    }
+    
+    func restartTodoList() {
+        todos.send([])
+        getTodoList()
+    }
+}
--- SimpleTodo/Screens/Home/HomeViewModelType.swift
+++ SimpleTodo/Screens/Home/HomeViewModelType.swift
@@ -16,10 +16,28 @@ typealias HomeViewModelType = HomeViewModelInput & HomeViewModelOutput
 
 protocol HomeViewModelInput {
     // user inputs & actions
+    var search: CurrentValueSubject<String, Never> { get set }
+    func getPlaceholders(search: String)
+    func getTodoList(search: String)
+    func addTodo(_ todo: TodoListItemModel)
+    func deleteTodo(_ todo: TodoListItemModel)
+    func toggleCompleted(_ todo: TodoListItemModel, isCompleted: Bool)
 }
 
 //MARK: - Output
  
 protocol HomeViewModelOutput {
     // state changes & results
+    var placeholders: CurrentValueSubject<[TodoListItemModel], Never> { get set }
+    var todos: CurrentValueSubject<[TodoListItemModel], Never> { get set }
+    var errorMessage: CurrentValueSubject<String, Never> { get }
+    var isLoading: CurrentValueSubject<Bool, Never> { get }
+    var isEmpty: Bool { get }
+    var numberOfSections: Int { get }
+    func headerTitle(section: Int) -> String?
+    func numberOfCells(section: Int) -> Int
+    func heightForRow(section: Int) -> CGFloat
+    func todo(section: Int, index: Int) -> TodoListItemModel?
+    func restartPlaceholders()
+    func restartTodoList()
 }
